862d428a4d7e8d5dd22fcdcf743ebe3132d63186
TheBigTL <thienlac1234@outlook.com>
2025-10-21 15:23:48 -0700

    Update exclude.py
    Added a while loop and some if conditionals

--- exclude.py
+++ exclude.py
@@ -1,4 +1,17 @@
 
+phone = True
+while phone:
+    user_input = input("True or false: Thienlac is cool: ")
+    if user_input == "True" or user_input == "true" or user_input == "t":
+        print("That's what I thought.")
+        phone = False
+    if user_input == "False" or user_input == "false" or user_input == "f":
+        print("No, Thienlac is cool.")
+        
+        
+
+
+
 dog = True
 like_count = 0  # Initialize a counter to track how many times the user says "yes"
 roblox = 100000
